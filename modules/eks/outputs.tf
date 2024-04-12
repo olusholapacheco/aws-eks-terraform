858d650e1b3142353e2fec7419bcf0d7a3800992
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-12 10:21:05 +0000

    outputs.tf updates

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -10,20 +10,20 @@ output "eks_cluster_endpoint" {
 
 output "eks_cluster_security_group_ids" {
   description = "Security group IDs attached to the EKS cluster"
-  value       = module.eks.cluster_security_group_ids
+  value       = aws_security_group.eks_cluster_security_group_ids[*].id
 }
 
 output "eks_cluster_subnet_ids" {
   description = "Subnet IDs associated with the EKS cluster"
-  value       = module.eks.cluster_subnet_ids
+  value       = aws_subnet.eks_cluster_subnet_ids[*].id
 }
 
 output "eks_node_group_one_instance_ids" {
   description = "EC2 instance IDs of node group one"
-  value       = module.eks.node_group_one_instance_ids
+  value       = aws_instance.eks_node_group_one_instance_ids[*].id
 }
 
 output "eks_node_group_two_instance_ids" {
   description = "EC2 instance IDs of node group two"
-  value       = module.eks.node_group_two_instance_ids
+  value       = aws_instance.eks_node_group_two_instance_ids[*].id
 }
