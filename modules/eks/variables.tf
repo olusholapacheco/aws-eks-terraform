c573c6e8e6b1b8bc2671e2d2fdd2756aa035ed25
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-12 09:58:58 +0000

    variables.tf

---
+++ modules/eks/variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Name of associated EKS Cluster"
+  type        = string 
+  default     = "Cluster1"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version. Defaults to EKS Cluster Kubernetes version"
+  type        = string
+  default     = "1.23"  
+}
+
+variable "vpc_id" {
+  description = "ID of Default AWS VPC"
+  type        = string
+}
+
+
