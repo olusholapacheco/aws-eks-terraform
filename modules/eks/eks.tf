f898abad1bbfe9be734f7bc38a367dfa879a1988
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-18 07:32:57 +0000

    eks.tf

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -4,7 +4,6 @@ module "eks" {
 
   cluster_name                   = var.cluster_name
   cluster_version                = var.cluster_version
-  subnet_ids                     = var.subnet_ids
   vpc_id                         = var.vpc_id
   cluster_endpoint_public_access = true
 
