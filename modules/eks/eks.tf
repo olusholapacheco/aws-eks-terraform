2c3db1b5d4e46881f069ad9264a8f24e279231c0
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-19 05:06:45 +0000

    update

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -4,7 +4,7 @@ module "eks" {
 
   cluster_name                   = var.cluster_name
   cluster_version                = var.cluster_version
-  subnet_ids                     = ["subnet-0576f3a427015ceb8", "subnet-0322f6484a69c43ed"]
+  subnet_ids                     = ["subnet-0576f3a427015ceb8", "subnet-0322f6484a69c43ed","subnet-0c1d1f8d0756bccc3"]
   vpc_id                         = var.vpc_id
   cluster_endpoint_public_access = true
 
@@ -16,9 +16,9 @@ module "eks" {
     one = {
       instance_type = ["t3.medium"] 
       name         = "node-group-3"
-      min_size     = 2
-      max_size     = 3
-      desired_size = 3
+      min_size     = 1
+      max_size     = 2
+      desired_size = 2
     }
 
     two = {
