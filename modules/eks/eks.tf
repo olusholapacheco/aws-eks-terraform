7a6002a4f68aac3f94163ddc4b212848e091c0ca
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-22 08:53:36 +0000

    update

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -30,3 +30,11 @@ module "eks" {
     }
   }
 }
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
