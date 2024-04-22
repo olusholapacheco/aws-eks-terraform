3ebd70271a52bb225c48f2318c0335bc6c74afeb
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-22 12:22:32 +0000

    s3 bucket update

--- modules/eks/backend.tf
+++ modules/eks/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "cluster1statefile"
-    key            = "mycluster1statefile/terraform.tfstate"
+    bucket         = "cluster1statefileupdate"
+    key            = "mycluster1statefileupdate/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock2"
   }
