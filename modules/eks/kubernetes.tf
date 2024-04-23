8218e78b77dca26b7d9edac78fe466388f4c638f
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-23 08:30:08 +0000

    helm

--- modules/eks/kubernetes.tf
+++ modules/eks/kubernetes.tf
@@ -10,3 +10,22 @@ resource "kubernetes_namespace" "nginx" {
     name = "nginx-ingress"
   }
 }
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = kubernetes_namespace.nginx.metadata[0].name
+
+  set {
+    name  = "controller.ingressClassName"
+    value = "learn"
+  }
+}
+
