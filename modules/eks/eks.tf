4b0382f1fababd1c8e61fca424452ada55ae654f
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-25 19:12:57 +0000

    update

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -33,7 +33,7 @@ module "eks" {
 
 
 resource "aws_cloudwatch_log_group" "Cluster-1_eks_cluster_log_group" {
-  name              = "/aws/eks/Cluster-1/cluster"
+  name              = "/aws/eks/Cluster-1/cluster1"
   retention_in_days = 0  # Set retention value to not keep logs for now
   
 }
