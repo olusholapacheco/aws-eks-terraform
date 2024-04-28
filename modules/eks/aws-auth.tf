efd246c54ff1bee53ffb9371a27ee450ff0fe3dc
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-28 11:58:01 +0000

    aws-auth.tf

---
+++ modules/eks/aws-auth.tf
@@ -0,0 +1,26 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "~> 20.0"
+
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::960509790187:role/Cluster-1-cluster-20240425114237968200000005"
+      username = "Cluster-1-cluster-20240425114237968200000005" # You find this username by clicking on the Role ARN from the eks cluster, it takes you to IAM where you can see it.Since one is building via terraform
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::960509790187:user/audit"
+      username = "audit"
+      groups   = ["read-only-users"]
+    },
+  ]
+
+  aws_auth_accounts = [
+    "960509790187"
+  ]
+}
