dab99b4a57324ed25b629e21597c75cbee6a5df6
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-28 11:45:58 +0000

    group.tf

--- modules/eks/group.tf
+++ modules/eks/group.tf
@@ -1,5 +1,18 @@
-resource "kubernetes_group" "read_only_users" {
+resource "kubernetes_role_binding" "read_only_users" {
   metadata {
-    name = "read-only-users"
+    name      = "read-only-users"
+    namespace = "nginx-ingress"
+  }
+
+  role_ref {
+    name      = "view"
+    kind      = "ClusterRole"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind      = "Group"
+    name      = "read-only-users"
+    namespace = "nginx-ingress"
   }
 }
