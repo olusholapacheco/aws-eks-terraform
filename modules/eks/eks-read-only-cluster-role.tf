db275ba18fdf8982bcdd5dcf70e592afb90e59e5
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-28 11:06:35 +0000

    edits

--- modules/eks/eks-read-only-cluster-role.tf
+++ modules/eks/eks-read-only-cluster-role.tf
@@ -7,7 +7,8 @@ resource "kubernetes_cluster_role" "eks_readonly_cluster_role" {
     api_groups = [""]
     resources  = ["pods"]
     verbs      = ["get", "list", "watch"]
-    resource_names = [""]  # Allow access to all pods
+    resource_names = [""]  # Allow access to all pods within the namespace
+    # namespace = "nginx-ingress"  # Limit access to the nginx-ingress namespace
   }
 }
 
