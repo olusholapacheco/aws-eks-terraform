35be00286d7b8fcae2a000a2df58018cb948b159
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-28 11:02:45 +0000

    updates

--- modules/eks/eks-read-only-cluster-role.tf
+++ modules/eks/eks-read-only-cluster-role.tf
@@ -7,5 +7,25 @@ resource "kubernetes_cluster_role" "eks_readonly_cluster_role" {
     api_groups = [""]
     resources  = ["pods"]
     verbs      = ["get", "list", "watch"]
+    resource_names = [""]  # Allow access to all pods within the namespace
+    namespace = "nginx-ingress"  # Limit access to the nginx-ingress namespace
   }
 }
+
+resource "kubernetes_cluster_role_binding" "eks_readonly_user_binding" {
+  metadata {
+    name = "eks-readonly-user-binding"
+  }
+
+  role_ref {
+    kind     = "ClusterRole"
+    name     = kubernetes_cluster_role.eks_readonly_cluster_role.metadata[0].name
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind = "User"
+    name = "audit"
+  }
+}
+
