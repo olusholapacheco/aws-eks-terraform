4b5281b345f6e13c2f110d5ad683f970bbaa93be
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-04-30 18:59:37 +0000

    logging - turn off

--- modules/eks/log.tf
+++ modules/eks/log.tf
@@ -2,7 +2,17 @@ resource "aws_eks_cluster" "Cluster-1" {
   name     = var.cluster_name
   role_arn = "arn:aws:iam::960509790187:role/Cluster-1-cluster-20240425114237968200000005"
 
-  cluster_logging {
-    enabled = false
+  vpc_config {
+    subnet_ids = ["subnet-08ba09b2f0fe304ce", "subnet-0b6dcafd6c9d2e499", "subnet-042b894f03eac2ff2"]
+  }
+}
+
+resource "aws_eks_cluster_update" "Cluster-1" {
+  name = var.cluster_name
+
+  logging {
+    cluster_logging {
+      enabled = false
+    }
   }
 }
